2921036ba42835e6823f0810df67a8a734d49063
mayaknayak <36425957+mayaknayak@users.noreply.github.com>
2018-11-29 15:21:13 -0500

    Edited unload_box.cpp to rename bad object and dispose of all objects in box

---
+++ unload_box.cpp
@@ -0,0 +1,164 @@
+//unload_box.cpp:
+// moves a box under camera, then removes sensed parts from box
+
+//use a RobotBehaviorInterface object to communicate with the robot behavior action server
+#include <robot_behavior_interface/RobotBehaviorInterface.h>
+
+//we define a message type for a "part" that includes name, pose and associated location code (e.g. bin # or box location)
+#include<inventory_msgs/Part.h>
+
+//a "box inspector" object can compare a packing list to a logical camera image to see how we are doing
+#include<box_inspector/box_inspector.h>
+
+//conveyor interface communicates with the conveyor action server
+#include<conveyor_as/ConveyorInterface.h>
+
+
+
+const double COMPETITION_TIMEOUT=500.0; // need to  know what this is for the finals;
+// want to ship out partial credit before time runs out!
+
+void model_to_part(osrf_gear::Model model, inventory_msgs::Part &part, unsigned short int location) {
+    part.name = model.type;
+    part.pose.pose = model.pose;
+    part.location = location; //by default
+}
+
+int main(int argc, char** argv) {
+    // ROS set-ups:
+    ros::init(argc, argv, "box_unloader"); //node name
+    ros::NodeHandle nh; // create a node handle; need to pass this to the class constructor
+    int ans;
+    
+ 
+    ROS_INFO("instantiating a RobotBehaviorInterface");
+    RobotBehaviorInterface robotBehaviorInterface(&nh); //shipmentFiller owns one as well
+
+    ROS_INFO("instantiating a ConveyorInterface");
+    ConveyorInterface conveyorInterface(&nh);
+   
+    ROS_INFO("instantiating a BoxInspector");
+    BoxInspector boxInspector(&nh);
+
+    //instantiate an object of appropriate data type for our move-part commands
+    inventory_msgs::Part current_part;
+
+    geometry_msgs::PoseStamped box_pose_wrt_world;  //camera sees box, coordinates are converted to world coords
+    
+    bool status;    
+    int nparts;
+
+    //for box inspector, need to define multiple vectors for args, 
+    //box inspector will identify parts and convert their coords to world frame
+    //in the present example, desired_models_wrt_world is left empty, so ALL observed parts will be considered "orphaned"
+        vector<osrf_gear::Model> desired_models_wrt_world;
+        vector<osrf_gear::Model> satisfied_models_wrt_world;
+        vector<osrf_gear::Model> misplaced_models_actual_coords_wrt_world;
+        vector<osrf_gear::Model> misplaced_models_desired_coords_wrt_world;
+        vector<osrf_gear::Model> missing_models_wrt_world;
+        vector<osrf_gear::Model> orphan_models_wrt_world;
+        vector<int> part_indices_missing;
+        vector<int> part_indices_misplaced;
+        vector<int> part_indices_precisely_placed;
+
+    
+    //use conveyor action  server for multi-tasking
+    ROS_INFO("getting a box into position: ");
+    int nprint = 0;
+    conveyorInterface.move_new_box_to_Q1();  //member function of conveyor interface to move a box to inspection station 1
+    while (conveyorInterface.get_box_status() != conveyor_as::conveyorResult::BOX_SEEN_AT_Q1) {
+        ros::spinOnce();
+        ros::Duration(0.1).sleep();
+        nprint++;
+        if (nprint % 10 == 0) {
+            ROS_INFO("waiting for conveyor to advance a box to Q1...");
+        }
+    }
+
+    //update box pose,  if possible              
+    if (boxInspector.get_box_pose_wrt_world(box_pose_wrt_world)) {
+        ROS_INFO_STREAM("box seen at: " << box_pose_wrt_world << endl);
+    }
+    else {
+        ROS_WARN("no box seen.  something is wrong! I quit!!");
+        exit(1);
+    }
+    
+    // if survive to here, then box is at Q1 inspection station; 
+    
+    //inspect the box and classify all observed parts
+    boxInspector.update_inspection(desired_models_wrt_world,
+        satisfied_models_wrt_world,misplaced_models_actual_coords_wrt_world,
+        misplaced_models_desired_coords_wrt_world,missing_models_wrt_world,
+        orphan_models_wrt_world,part_indices_missing,part_indices_misplaced,
+        part_indices_precisely_placed);
+    ROS_INFO("orphaned parts in box: ");
+    nparts = orphan_models_wrt_world.size();
+    ROS_INFO("num parts seen in box = %d",nparts);
+    for (int i=0;i<nparts;i++) {
+       ROS_INFO_STREAM("orphaned  parts: "<<orphan_models_wrt_world[i]<<endl);
+    }
+    
+    
+
+    if (boxInspector.get_bad_part_Q1(current_part)) {
+
+	// LOOP THROUGH ORPHANS TO FIND COORDINATES THAT MATCH WITH BAD PART'S COORDINATES
+	for (int i = 0; i < orphan_models_wrt_world.size(); i++)
+	{
+	    if (abs(orphan_models_wrt_world[i].pose.position.x - current_part.pose.pose.position.x) < .01
+  	 	&& abs(orphan_models_wrt_world[i].pose.position.y - current_part.pose.pose.position.y) < .01
+		&& abs(orphan_models_wrt_world[i].pose.position.z - current_part.pose.pose.position.z) < .01)
+	    {
+		current_part.name = orphan_models_wrt_world[i].type;
+            }
+	}
+        ROS_INFO("found bad part: ");
+        ROS_INFO_STREAM(current_part<<endl);
+        
+        cout<<"enter 1 to attempt to remove bad part: "; //poor-man's breakpoint
+        cin>>ans;       
+	
+	// DISPOSE BAD PART
+	robotBehaviorInterface.pick_part_from_box(current_part); 
+
+    }    
+
+    //after removing the bad part, re-inspect the box:
+    boxInspector.update_inspection(desired_models_wrt_world,
+        satisfied_models_wrt_world,misplaced_models_actual_coords_wrt_world,
+        misplaced_models_desired_coords_wrt_world,missing_models_wrt_world,
+        orphan_models_wrt_world,part_indices_missing,part_indices_misplaced,
+        part_indices_precisely_placed);
+    ROS_INFO("orphaned parts in box: ");
+    nparts = orphan_models_wrt_world.size();
+    ROS_INFO("num parts seen in box = %d",nparts);
+    for (int i=0;i<nparts;i++) {
+       ROS_INFO_STREAM("orphaned  parts: "<<orphan_models_wrt_world[i]<<endl);
+       // REMOVE ALL THE OTHER PARTS IN THE BOX
+       //void model_to_part(osrf_gear::Model model, inventory_msgs::Part &part, unsigned short int location) 
+       model_to_part(orphan_models_wrt_world[i], current_part, inventory_msgs::Part::QUALITY_SENSOR_1);
+
+       //use the robot action server to acquire and dispose of the specified part in the box:
+       status = robotBehaviorInterface.pick_part_from_box(current_part);
+    }
+
+
+
+
+
+    // move robot to grasp and discard each part
+
+    //start w/ first part
+
+    //box inspector sees "model", defined in osrf_gear; convert this to our datatype "Part"
+    
+
+    //SHOULD REPEAT FOR ALL THE PARTS IN THE BOX
+    //ALSO, WATCH OUT FOR NO PARTS IN THE BOX--ABOVE WILL CRASH
+
+            return 0;
+    //here's an oddity: this node runs to completion.  But sometimes, Linux complains bitterly about
+    // *** Error in `/home/wyatt/ros_ws/devel/lib/shipment_filler/unload_box': corrupted size vs. prev_size: 0x000000000227c7c0 ***
+    // don't know why.  But does not seem to matter.  If anyone figures this  out, please let me know.
+}
